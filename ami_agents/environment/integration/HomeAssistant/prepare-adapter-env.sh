a43a4d2ea798212b0968a7d31c875e928878e180
icordos <icordos@gmail.com>
2026-05-10 18:31:35 +0300

    Rectify BASE_WS_URI

--- ami_agents/environment/integration/HomeAssistant/prepare-adapter-env.sh
+++ ami_agents/environment/integration/HomeAssistant/prepare-adapter-env.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-export AREAS="lab308"
+#export AREAS="lab308"
+export AREAS="lab308e"
 #export AREAS="qt1feasibleseed1"
-export BASE_WS_URI="http://localhost:8008/"
+export BASE_WS_URI="http://localhost:8080/"
 export HASP_URL="http://localhost:8080/"
 export HA_TOKEN="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI2MTcxYmQ1NGRiNDA0NmRiYjYxNWI1MDRkYTA4ZWM5YiIsImlhdCI6MTc3MjE5NzI0MCwiZXhwIjoyMDg3NTU3MjQwfQ.tWqiBqib3Su9SNFWppghGpHmsv4KAfhRqV1gmTP2UEo"
 export HA_URL="ws://localhost:8123/api/websocket"
